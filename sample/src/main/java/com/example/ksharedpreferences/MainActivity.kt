2e3ca2174c3002e6b31754805267af3d0ca48537
alexk <gklymoxa@gmail.com>
2019-03-15 19:15:18 +0200

    Implemented simple example, update version

--- sample/src/main/java/com/example/ksharedpreferences/MainActivity.kt
+++ sample/src/main/java/com/example/ksharedpreferences/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.ksharedpreferences
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.alexklab.ksharedpreferences.KSharedPreference
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val property = KSharedPreference()
+        Log.d("onCreate", "lastUsageTime: ${Date(Prefs.lastUsageTime)}")
+        Log.d("onCreate", "userID: ${Prefs.userID}")
+        Prefs.lastUsageTime = Date().time
     }
+}
+
+object Prefs {
+
+    var userID by KSharedPreference(defaultValue = "NONE")
+    var lastUsageTime: Long by KSharedPreference(defaultValue = 0L)
 }

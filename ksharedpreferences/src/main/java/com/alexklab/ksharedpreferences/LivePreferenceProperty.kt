faa650d6ef457ce4110cd80012d3482f42a2d223
alexk <gklymoxa@gmail.com>
2020-01-23 15:07:53 +0200

    refactoring for LivePreferenceProperty update sample

--- ksharedpreferences/src/main/java/com/alexklab/ksharedpreferences/LivePreferenceProperty.kt
+++ ksharedpreferences/src/main/java/com/alexklab/ksharedpreferences/LivePreferenceProperty.kt
@@ -1,5 +1,7 @@
 package com.alexklab.ksharedpreferences
 
+import android.content.SharedPreferences
+import androidx.lifecycle.MutableLiveData
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
@@ -14,9 +16,10 @@ import kotlin.reflect.KProperty
 class LivePreferenceProperty<T : Any>(
     defaultValue: T,
     key: String? = null
-) : PreferenceProperty<T>(defaultValue, key), ReadOnlyProperty<PreferencesHolder, LivePreference<T>> {
+) : PreferenceProperty<T>(defaultValue, key),
+    ReadOnlyProperty<PreferencesHolder, MutableLiveData<T>> {
 
-    private lateinit var livePreference: LivePreference<T>
+    private lateinit var liveData: MutableLiveData<T>
 
     /**
      * Returns the value of the LivePreference property with registered liveData listener.
@@ -25,16 +28,52 @@ class LivePreferenceProperty<T : Any>(
      * @param property the metadata for the property.
      * @return the LivePreference.
      */
-    override fun getValue(thisRef: PreferencesHolder, property: KProperty<*>): LivePreference<T> {
-        if (::livePreference.isInitialized) {
-            livePreference.registerOnSharedPreferenceChangeListener()
-        } else {
-            livePreference = LivePreference(
-                key = getPreferenceKey(property),
-                defaultValue = defaultValue,
-                prefs = thisRef.prefs
-            )
+    override fun getValue(thisRef: PreferencesHolder, property: KProperty<*>): MutableLiveData<T> {
+        if (!::liveData.isInitialized) {
+            val prefKey = getPreferenceKey(property)
+            val prefs = thisRef.prefs.value
+            liveData = newPreferenceMutableLiveData(prefs, prefKey)
+            prefs.registerOnSharedPreferenceChangeListener(liveData, prefKey)
         }
-        return livePreference
+
+        return liveData
+    }
+
+    private fun newPreferenceMutableLiveData(
+        prefs: SharedPreferences,
+        prefKey: String
+    ): MutableLiveData<T> =
+        object : MutableLiveData<T>(prefs.getValue(prefKey, defaultValue)) {
+
+            override fun postValue(value: T) {
+                super.postValue(value)
+                updatePrefValue(value)
+            }
+
+            override fun setValue(value: T) {
+                super.setValue(value)
+                updatePrefValue(value)
+            }
+
+            @Suppress("USELESS_ELVIS")
+            private fun updatePrefValue(value: T) {
+                val givenValue = value ?: defaultValue // nullable types not supported
+                prefs.edit { putValue(prefKey, givenValue) }
+            }
+        }
+
+    private fun SharedPreferences.registerOnSharedPreferenceChangeListener(
+        data: MutableLiveData<T>,
+        prefKey: String
+    ) {
+
+        registerOnSharedPreferenceChangeListener { p, k ->
+            if (prefKey == k) {
+                val prefValue = p.getValue(prefKey, defaultValue)
+                if (prefValue != data.value) {
+                    data.value = prefValue
+                }
+            }
+        }
     }
 }
